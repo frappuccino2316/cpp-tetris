815fe57d69ce07f066ce22496e33213321e587a6
frappuccino2316 <frappuccino2316@gmail.com>
2024-02-17 01:17:16 +0900

    画面表示を別の関数に切り出す

--- main.cpp
+++ main.cpp
@@ -4,36 +4,44 @@
 #include <unistd.h>
 
 static const int HEIGHT = 20;
-static const int WIDTH = 10;
+static const int WIDTH = 12;
 
-char field[HEIGHT][WIDTH];
+int field[HEIGHT][WIDTH];
 
 void frame() {
-    std::fill(&field[0][0], &field[0][0] + HEIGHT * WIDTH, '0');
+    std::fill(&field[0][0], &field[0][0] + HEIGHT * WIDTH, 0);
 
     for (int i = 0; i < HEIGHT; ++i) {
-        field[i][0] = '1';
-        field[i][WIDTH - 1] = '1';
+        field[i][0] = 1;
+        field[i][WIDTH - 1] = 1;
     }
 
     for (int i = 0; i < WIDTH; ++i) {
-        field[HEIGHT - 1][i] = '1';
+        field[HEIGHT - 1][i] = 1;
+    }
+}
+
+void display() {
+    system("clear");
+
+    std::cout << "\n\n\n";
+    
+    for (int i = 0; i < HEIGHT; ++i) {
+        for (int j = 0; j < WIDTH; ++j) {
+            if (field[i][j] == 1) {
+                std::cout << " ■";
+            } else {
+                std::cout << "  ";
+            }
+        }
+        std::cout << std::endl;
     }
 }
 
 int main() {
     while (true) {
         frame();
-        system("clear");
-
-        std::cout << "テトリスっぽいゲーム\n";
-        
-        for (int i = 0; i < HEIGHT; ++i) {
-            for (int j = 0; j < WIDTH; ++j) {
-                std::cout << field[i][j];
-            }
-            std::cout << std::endl;
-        }
+        display();
 
         usleep(1000000);
     }
